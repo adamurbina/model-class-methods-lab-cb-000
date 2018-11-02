5caac9131d09c4040874406f857a13625bb2ac09
Adam Urbina <adamurbina@gmail.com>
2018-11-02 19:13:38 +0000

    automatically backed up by learn

--- app/models/classification.rb
+++ app/models/classification.rb
@@ -7,7 +7,7 @@ class Classification < ActiveRecord::Base
   end
 
   def self.longest
-    self.joins(:boats).group('length').having("length = Boats.maximum(:length)")
+    self.joins(:boats).group('length').having("length = Boat.maximum(:length)")
   end
 
 
