cbf20c822af61c4bad5f3fccb17beb4ac446da43
Adam Urbina <adamurbina@gmail.com>
2018-11-02 18:34:52 +0000

    automatically backed up by learn

--- app/models/captain.rb
+++ app/models/captain.rb
@@ -12,7 +12,7 @@ class Captain < ActiveRecord::Base
   end
 
   def self.talented_seafarers
-    self.joins(boats: :classifications).where("classifications.name = ? AND classifications.name = ?", "Sailboat", "Motorboat").distinct
+    self.joins(boats: :classifications).where(classifications: {name: "Sailboat"}).where(classifications: {name: "Motorboat"}).distinct
   end
 
   def self.non_sailors
