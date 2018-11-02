7b98cfc54501ae816f61ae652ad7a34bcedb5910
Adam Urbina <adamurbina@gmail.com>
2018-11-02 17:35:22 +0000

    automatically backed up by learn

--- app/models/boat_classification.rb
+++ app/models/boat_classification.rb
@@ -4,7 +4,7 @@ class BoatClassification < ActiveRecord::Base
 
   def self.classification_counts(num)
     boat_counts = self.group(:boat_id).count
-    matching_ids = boat_counts{|boat_id, boat_count| boat_count == num}
+    matching_ids = boat_counts.select{|boat_id, boat_count| boat_count == num}
     matching_ids.keys
   end
 
