9e5523287154b283903eea5b4632f2ff30cdad98
Adam Urbina <adamurbina@gmail.com>
2018-11-02 18:07:49 +0000

    automatically backed up by learn

--- app/models/captain.rb
+++ app/models/captain.rb
@@ -2,7 +2,7 @@ class Captain < ActiveRecord::Base
   has_many :boats
 
   def self.catamaran_operators
-    self.where(id: Boat.find_classification("Catamaran"))
+    self.where(id: Boat.find_classification("Catamaran").pluck(:boat_id))
   end
 
   def self.sailors
