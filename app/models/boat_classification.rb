c46bbff74553de06f603721df80adffa6f5be139
Adam Urbina <adamurbina@gmail.com>
2018-11-02 17:28:49 +0000

    automatically backed up by learn

--- app/models/boat_classification.rb
+++ app/models/boat_classification.rb
@@ -2,8 +2,10 @@ class BoatClassification < ActiveRecord::Base
   belongs_to :boat
   belongs_to :classification
 
-  def self.classification_counts
-    self.group(:boat_id).count
+  def self.classification_counts(num)
+    self.group(:boat_id).count.select do |key, value|
+      value == num
+    end
   end
 
 end
