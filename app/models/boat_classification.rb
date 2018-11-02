9102331cac036e8473a0ea0adfe8228ed60c60c5
Adam Urbina <adamurbina@gmail.com>
2018-11-02 17:22:11 +0000

    automatically backed up by learn

--- app/models/boat_classification.rb
+++ app/models/boat_classification.rb
@@ -2,7 +2,7 @@ class BoatClassification < ActiveRecord::Base
   belongs_to :boat
   belongs_to :classification
 
-  def self.count
+  def self.count_classifications
     self.group(:classification_id).count
   end
 
