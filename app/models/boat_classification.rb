5a056f1a52c9a9c3f309e54a47ceac3591e29fb2
Adam Urbina <adamurbina@gmail.com>
2018-11-02 17:01:21 +0000

    automatically backed up by learn

--- app/models/boat_classification.rb
+++ app/models/boat_classification.rb
@@ -2,7 +2,5 @@ class BoatClassification < ActiveRecord::Base
   belongs_to :boat
   belongs_to :classification
 
-  def self.sailboats
-    self.where(classification_id: Classification.sailboat.first.id)
-  end
+
 end
