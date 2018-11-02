e0a2060e60bf42f410b7f267634e306316bf2512
Adam Urbina <adamurbina@gmail.com>
2018-11-02 19:01:16 +0000

    automatically backed up by learn

--- app/models/captain.rb
+++ app/models/captain.rb
@@ -2,17 +2,14 @@ class Captain < ActiveRecord::Base
   has_many :boats
 
   def self.catamaran_operators
-    #self.where(id: Boat.find_classification("Catamaran").pluck(:captain_id))
     self.joins(boats: :classifications).where(classifications: {name: "Catamaran"}).distinct
   end
 
   def self.sailors
-    #self.where(id: Boat.find_classification("Sailboat").pluck(:captain_id))
     self.joins(boats: :classifications).where(classifications: {name: "Sailboat"}).distinct
   end
 
   def self.motorboaters
-    #self.where(id: Boat.find_classification("Sailboat").pluck(:captain_id))
     self.joins(boats: :classifications).where(classifications: {name: "Motorboat"}).distinct
   end
 
@@ -20,13 +17,10 @@ class Captain < ActiveRecord::Base
     motorboaters = self.motorboaters.pluck(:id)
     sailors = self.sailors.pluck(:id)
     self.where(id: [motorboaters & sailors])
-
-
-
   end
 
   def self.non_sailors
-    self.joins(boats: :classifications).where.not(classifications: {name: "Sailboat"}).distinct
+    self.joins(boats: :classifications).where(classifications: {name: !"Sailboat"}).distinct
   end
 
 end
