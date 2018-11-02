0754a846e94b95a3346cbf09dcf3e6b0cdbafc77
Adam Urbina <adamurbina@gmail.com>
2018-11-02 17:21:52 +0000

    automatically backed up by learn

--- app/models/boat_classification.rb
+++ app/models/boat_classification.rb
@@ -2,8 +2,8 @@ class BoatClassification < ActiveRecord::Base
   belongs_to :boat
   belongs_to :classification
 
-  def self.three_classifications
-    self.group(:classification_id).count(3)
+  def self.count
+    self.group(:classification_id).count
   end
 
 end
